e6651601a5390438b3f886ab206a0324671ecd9d
Merry-robot <elijahjubrey@gmail.com>
2025-02-11 20:21:20 -0500

    add framework for custom built website
    using svelte with typsecript and tailwind css

---
+++ 571Web/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<div id="topbar" class="shadow-xl">
+    <h1><center>Paragon Robotics</center></h1>
+</div>
+
+<div id="body">   
+    <center>
+    <h1 id="ext-top-text"> 571 </h1>
+    </center>
+</div>
+
+<div id="article-grid">
+    <div>
+        <center>
+        <div id="title-header"><h2>2025 Season Update</h2><h4>a</h4></div>
+        </center>
+    </div>
+    <div>
+        <center>
+        <div id="title-header"><h2>Community Can Drive</h2><h4>a</h4></div>
+        </center>
+    </div>
+</div>
