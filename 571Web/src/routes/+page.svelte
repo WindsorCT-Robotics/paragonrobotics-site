46747fdf840836131d7d26fcaea380df191dbb5e
Merry-robot <elijahjubrey@gmail.com>
2025-02-12 01:39:46 -0500

    reformat css and add footer

--- 571Web/src/routes/+page.svelte
+++ 571Web/src/routes/+page.svelte
@@ -1,22 +1,36 @@
-<div id="topbar" class="shadow-xl">
-    <h1><center>Paragon Robotics</center></h1>
-</div>
+<div id="content-wrapper" class="flex flex-col min-h-screen">
+    <div id="topbar" class="shadow-xl">
+        <h1 id="toptext"><center>Paragon Robotics</center></h1>
+    </div>
 
-<div id="body">   
-    <center>
-    <h1 id="ext-top-text"> 571 </h1>
-    </center>
-</div>
-
-<div id="article-grid">
-    <div>
+    <div id="topbar2">   
         <center>
-        <div id="title-header"><h2>2025 Season Update</h2><h4>a</h4></div>
+        <h1> 571 </h1>
         </center>
     </div>
-    <div>
-        <center>
-        <div id="title-header"><h2>Community Can Drive</h2><h4>a</h4></div>
-        </center>
+
+    <div id="article-grid">
+        <div>
+            <center>
+            <div id="title-header"><h2>2025 Season Update</h2><h4>a</h4></div>
+            </center>
+        </div>
+        <div>
+            <center>
+            <div id="title-header"><h2>Community Can Drive</h2><h4>a</h4></div>
+            </center>
+        </div>
+    </div>
+
+    <div id="footer">
+        <div id="f-images">
+            <h1>this is an image slideshow</h1>
+        </div>
+      
+        <div id="contact">
+            <h1>this is a contact form</h1>
+        </div>
+
+        <center>&copy; Paragon Robotics 2025</center>
     </div>
 </div>
