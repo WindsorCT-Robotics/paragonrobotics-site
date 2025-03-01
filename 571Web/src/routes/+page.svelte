7df6dd2bff3c8fe30da20277dffd954396b6fd2c
Merry-robot <elijahjubrey@gmail.com>
2025-03-01 14:06:14 -0500

    Add team logo
    need to format properly stilll

--- 571Web/src/routes/+page.svelte
+++ 571Web/src/routes/+page.svelte
@@ -1,6 +1,16 @@
+<script>
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import logo from '$lib/images/logo.png';
+</script>
+
 <div id="content-wrapper" class="flex flex-col min-h-screen">
-    <div id="bg" class="shadow-xl">
+    <div id="bg" class="shadow-sm">
         <div id="topbar"></div>
-        <h1 id="maintext">Paragon Robotics</h1>
+        <div id="logo-cont" class="flex justify-center items-center p-4 mt-16" style="height: calc(100vh - 4rem)"> 
+            <h1 id="maintext">
+                <img alt="Team Logo" src={logo} class="max-h-full">
+                Paragon Robotics
+            </h1>
+        </div>
     </div>
-</div> 
+</div>
